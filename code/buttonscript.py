a0ed17439bd1391ace3bb1b75e16498037a3ccc4
Jackson Williamson <104363831+fuzzyers@users.noreply.github.com>
2023-06-20 21:29:24 +1200

    Update buttonscript.py

--- code/buttonscript.py
+++ code/buttonscript.py
@@ -2,13 +2,35 @@
 import RPi.GPIO as GPIO
 import time
 import subprocess
+import ctypes
 
 GPIO.setmode(GPIO.BCM)
-button_pin = #unassigned currently
+button_pin = 14
 GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-def run_script():
-    subprocess.call("script command", shell=True)
+import RPi.GPIO as GPIO
+import time
+import subprocess
+
+GPIO.setmode(GPIO.BCM)
+button_pin = 14
+GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+is_script_running = False
+
+def run_script(channel):
+    global is_script_running
+
+    if not is_script_running:
+        is_script_running = True
+        subprocess.call("python ./python-i2c.py", shell=True)
+        is_script_running = False
+
+GPIO.add_event_detect(button_pin, GPIO.FALLING, callback=run_script, bouncetime=200)
+
+while True:
+    time.sleep(1)
+
 
 GPIO.add_event_detect(button_pin, GPIO.FALLING, callback=run_script, bouncetime=200)
 
