6edd327d1b0b37f6e4854f393c6da95a9186c618
Jackson Williamson <104363831+fuzzyers@users.noreply.github.com>
2023-06-20 21:28:00 +1200

    Update python-i2c.py

--- code/python-i2c.py
+++ code/python-i2c.py
@@ -16,24 +16,28 @@ import picamera
 import os
 from time import sleep
 from datetime import date
+from datetime import datetime
+
+
 
 # Initialize camera
 camera = picamera.PiCamera()
 camera.resolution = (1920, 1080)
-camera.start_preview()
+#camera.start_preview()
 
 # Initialize I2C
 bus = smbus.SMBus(1)
 arduino_address = 0x12
 value_to_send = 0
 data_received = 0
+photos = 0
 
 # Define folder path
-folder_name = date.today().strftime("%d-%m-%Y")
+folder_name = datetime.now().strftime("%d-%m-%Y_%H-%M")
 folder_path = '../pictures/' + folder_name
 # Create folder if it doesn't exist
-os.mkdir(folder_path, exist_ok=True)
-
+if not os.path.exists(folder_path):
+    os.makedirs(folder_path)
 # Define states
 STATE_IDLE = 0
 STATE_MOTOR_TURN = 1
@@ -50,24 +54,35 @@ def motorturn():
     if current_state == STATE_MOTOR_TURN:
         bus.write_byte(arduino_address, value_to_send)
         data_received = bus.read_byte(arduino_address)
-        current_state = STATE_TAKE_PHOTO
+        if data_received == 1:
+            bus.write_byte(arduino_address, 0)
+            current_state = STATE_TAKE_PHOTO
 
 # Function takephoto takes a photo and saves it to the pictures folder.
 # The pictures folder is named after the current date.
 def takephoto():
-    global current_state, data_received
+    global current_state, data_received, photos
 
-    if current_state == STATE_TAKE_PHOTO and data_received == 0:
-        sleep(10)
+    if current_state == STATE_TAKE_PHOTO:
+        bus.write_byte(arduino_address, 0)
+        sleep(1)
         file_name = "photo_" + str(int(time.time())) + ".jpg"
         file_path = os.path.join(folder_path, file_name)
-        camera.capture(file_path)   
-        current_state = STATE_IDLE
+        camera.capture(file_path)
+        photos += 1
+        current_state = STATE_MOTOR_TURN
 
 while True:
     if current_state == STATE_IDLE:
         value_to_send = 1
         current_state = STATE_MOTOR_TURN
+        
+    if current_state == STATE_MOTOR_TURN:
+        motorturn()
+        #13 Total Rvolutions
+    if current_state == STATE_TAKE_PHOTO:
+        takephoto()
+        
+    if photos == 13:
+        break
 
-    motorturn()
-    takephoto()
